6d07338663451de63cc63cd6562ce9851fdc8462
wellington felix <wellington_bhmg@hotmail.com>
2020-05-20 13:08:26 -0300

    trabalhando no crud-mvc.

--- crud-mvc/app/core/model/task.model.js
+++ crud-mvc/app/core/model/task.model.js
@@ -1,5 +1,45 @@
-class TaskModel {
+export default class TaskModel {
 
-    constructor() { }
+    constructor() {
+        this.tasks = [
+            {
+                id: 1,
+                text: 'Acordar às 05:00:00 da manhã',
+                complete: false
+            },
+            {
+                id: 2,
+                text: 'Finalizar a unidade 03 da pós graduação',
+                complete: false
+            }
+        ];
+    }
     
+    createTask(taskText) {
+        const task = {
+            id: (this.tasks.length > 0) ? this.tasks[this.tasks.length - 1].id + 1 : 1,
+            text: taskText,
+            complete: false
+        };
+
+        this.tasks.push(task);
+    }
+
+    updateTask(id, updatedTaskText) {
+        this.tasks = this.tasks.map(task => {
+            if (task.id === id) {
+                task.text = updatedTaskText;
+                task.complete = task.complete;
+            }
+
+            return task;
+        });
+    }
+
+    deleteTask(id) {
+        this.tasks = this.tasks.filter(task => {
+            task.id !== id;
+        });
+    }
+
 }
---
+++ crud-mvc/assets/js/main.js
@@ -0,0 +1,9 @@
+import * as TaskModel from '../../app/core/model/task.model';
+
+window.addEventListener('load', () => {
+  const model = new TaskModel();
+  //const view = new TaskView();
+  //const controller = new TaskController(model, view);
+
+  console.log(model);
+});
