bfd89fab1c04b78c7ead72bbb98a66e0429f9c9d
Wellington Felix <wellington.felix@avancoinfo.com.br>
2020-06-04 14:27:15 -0300

    retornando uma resposta para a rota /users, instalando dependências e criando projeto react.

--- rocketseat/server/src/server.ts
+++ rocketseat/server/src/server.ts
@@ -2,8 +2,19 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', () => {
-    console.log('Listagem de Usuários');
+app.get('/users', (request, response) => {
+    const users = [{
+        name: 'wellington',
+        surname: 'felix'
+    }, {
+        name: 'palloma',
+        surname: 'felix'
+    }, {
+        name: 'maria',
+        surname: 'rita'
+    }];
+
+    response.json(users);
 });
 
 app.listen(3333);
